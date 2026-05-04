94cca0ef33a2308efcc747d2cecf015ec6583cb7
Robin Adamietz <adamietz@tuta.io>
2026-05-04 18:09:52 +0200

    fix linting error

--- env.d.ts
+++ env.d.ts
@@ -2,6 +2,6 @@
 
 declare module '*.vue' {
   import { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
+  const component: DefineComponent<object, object, unknown>
   export default component
 }
